348c7376b7ca50c3d379a49a22476567fd85f014
Taseen <tahseen0amin@gmail.com>
2015-02-16 23:58:17 +0000

    part 4 try complete

--- app/src/main/java/com/tut/app/ApiConnector.java
+++ app/src/main/java/com/tut/app/ApiConnector.java
@@ -4,14 +4,20 @@ import android.util.Log;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 /**
  * Created by tahseen0amin on 16/02/2014.
@@ -133,8 +139,33 @@ public class ApiConnector {
         return jsonArray;
     }
 
+    public boolean UploadImageToServer(List<NameValuePair> params) {
 
+        String url = "http://192.168.0.9/php/uploadImage.php";
+        HttpEntity httpEntity = null;
+        Log.e("Post Params", params.toString());
 
+        // Making HTTP request
+        try {
+            // defaultHttpClient
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
 
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            httpEntity = httpResponse.getEntity();
 
+            String entityResponse = EntityUtils.toString(httpEntity);
+            Log.e("Entity Response", entityResponse);
+            return true;
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
--- app/src/main/java/com/tut/app/CustomerDetailsActivity.java
+++ app/src/main/java/com/tut/app/CustomerDetailsActivity.java
@@ -1,13 +1,19 @@
 package com.tut.app;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,15 +21,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomerDetailsActivity extends Activity {
 
@@ -33,11 +48,14 @@ public class CustomerDetailsActivity extends Activity {
 
     private int CustomerID;
 
-
     //
     private ImageView picture;
     private static final String baseUrlForImage = "http://192.168.0.9/php/images/";
 
+    private Button changeImageButton;
+    private static final int SELECT_PICTURE = 1;
+    private String selectedImagePath;
+    private String idOfCustomer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +68,6 @@ public class CustomerDetailsActivity extends Activity {
 
         this.picture = (ImageView) this.findViewById(R.id.pic);
 
-
         // get Customer ID
         this.CustomerID = getIntent().getIntExtra("CustomerID", -1);
 
@@ -60,7 +77,21 @@ public class CustomerDetailsActivity extends Activity {
             new GetCustomerDetails().execute(new ApiConnector());
         }
 
+        // Change Image
+        this.changeImageButton = (Button) this.findViewById(R.id.changeImage);
 
+        this.changeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(CustomerDetailsActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), SELECT_PICTURE);
+            }
+        });
     }
 
 
@@ -88,6 +119,8 @@ public class CustomerDetailsActivity extends Activity {
                 String urlForImage = baseUrlForImage + customer.getString("imageName");
                 new DownloadImageTask(picture).execute(urlForImage);
 
+                idOfCustomer = customer.getString("id");
+
             }
             catch (Exception e)
             {
@@ -98,6 +131,95 @@ public class CustomerDetailsActivity extends Activity {
     }
 
 
+
+
+    public void SetImage(Bitmap newImage){
+        // set image to ImageView
+        this.picture.setImageBitmap(newImage);
+
+        // upload image to server
+        // encode bitmap
+        String imageData = encodeTobase64(newImage);
+        final List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("CustomerID", idOfCustomer));
+        params.add(new BasicNameValuePair("image", imageData));
+
+        new AsyncTask<ApiConnector,Long,Boolean>(){
+
+            @Override
+            protected Boolean doInBackground(ApiConnector... apiConnectors) {
+
+                return apiConnectors[0].UploadImageToServer(params);
+            }
+
+            @Override
+            protected void onPostExecute(Boolean aBoolean) {
+
+            }
+        }.execute(new ApiConnector());
+    }
+
+    public static String encodeTobase64(Bitmap image)
+    {
+        System.gc();
+
+        if (image == null)return null;
+
+        Bitmap immagex = image;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        immagex.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+        byte[] b = baos.toByteArray();
+
+        String imageEncoded = Base64.encodeToString(b, Base64.DEFAULT); // min minSdkVersion 8
+
+        return imageEncoded;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                if (Build.VERSION.SDK_INT < 19) {
+                    selectedImagePath = getPath(selectedImageUri);
+                    Bitmap bitmap = BitmapFactory.decodeFile(selectedImagePath);
+                    SetImage(bitmap);
+                } else {
+                    ParcelFileDescriptor parcelFileDescriptor;
+                    try {
+                        parcelFileDescriptor = getContentResolver().openFileDescriptor(selectedImageUri, "r");
+                        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                        parcelFileDescriptor.close();
+                        SetImage(image);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * helper to retrieve the path of an image URI
+     */
+    public String getPath(Uri uri) {
+        if( uri == null ) {
+            return null;
+        }
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        return uri.getPath();
+    }
 
 
 
